8738aa0f53da180dbaa7c4d2598224da2fc5d217
thanhquy119 <95570037+thanhquy119@users.noreply.github.com>
2026-07-30 21:43:38 +0700

    Nạp tối ưu trải nghiệm tải và mở file

--- app/transfer/layout.tsx
+++ app/transfer/layout.tsx
@@ -3,11 +3,13 @@ import "./qr-scanner.css";
 import "./icon-reset.css";
 import "./reader-overrides.css";
 import "./transfer-table-format.css";
+import "./transfer-ux.css";
 import "./spreadsheet-workspace.css";
 import PwaContextEnhancer from "./pwa-context-enhancer";
 import QrScannerEnhancer from "./qr-scanner-enhancer";
 import TableFormatEnhancer from "./table-format-enhancer";
 import TransferPolishEnhancer from "./transfer-polish-enhancer";
+import TransferUxEnhancer from "./transfer-ux-enhancer";
 import SpreadsheetWorkspaceEnhancer from "./spreadsheet-workspace-enhancer";
 
 export default function TransferLayout({ children }: { children: ReactNode }) {
@@ -18,6 +20,7 @@ export default function TransferLayout({ children }: { children: ReactNode }) {
       <QrScannerEnhancer />
       <TableFormatEnhancer />
       <TransferPolishEnhancer />
+      <TransferUxEnhancer />
       <SpreadsheetWorkspaceEnhancer />
     </>
   );
