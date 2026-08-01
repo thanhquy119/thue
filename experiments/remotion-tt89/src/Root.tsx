3315bc00920fb7a7c19299c82618320d5f4e0fe7
thanhquy119 <95570037+thanhquy119@users.noreply.github.com>
2026-08-01 19:10:54 +0700

    Giữ composition demo tương thích dữ liệu cũ

--- experiments/remotion-tt89/src/Root.tsx
+++ experiments/remotion-tt89/src/Root.tsx
@@ -1,6 +1,6 @@
 import {Composition} from 'remotion';
 import {ThongTu89} from './ThongTu89';
-import {COMPOSITION_ID, FPS, TOTAL_FRAMES} from './data';
+import {FPS, TOTAL_FRAMES} from './data';
 import {
   LegalVideo,
   calculateLegalVideoMetadata,
@@ -10,7 +10,7 @@ import {
 export const RemotionRoot = () => (
   <>
     <Composition
-      id={COMPOSITION_ID}
+      id="ThongTu89"
       component={ThongTu89}
       durationInFrames={TOTAL_FRAMES}
       fps={FPS}
