6757a7b0a95b278e4a65ff8166f9dd67b249ff6d
thanhquy119 <95570037+thanhquy119@users.noreply.github.com>
2026-08-01 18:33:53 +0700

    Đăng ký composition LegalVideo nhận input động

--- experiments/remotion-tt89/src/Root.tsx
+++ experiments/remotion-tt89/src/Root.tsx
@@ -1,14 +1,31 @@
 import {Composition} from 'remotion';
 import {ThongTu89} from './ThongTu89';
 import {COMPOSITION_ID, FPS, TOTAL_FRAMES} from './data';
+import {
+  LegalVideo,
+  calculateLegalVideoMetadata,
+  defaultLegalVideoProps,
+} from './LegalVideo';
 
 export const RemotionRoot = () => (
-  <Composition
-    id={COMPOSITION_ID}
-    component={ThongTu89}
-    durationInFrames={TOTAL_FRAMES}
-    fps={FPS}
-    width={1080}
-    height={1920}
-  />
+  <>
+    <Composition
+      id={COMPOSITION_ID}
+      component={ThongTu89}
+      durationInFrames={TOTAL_FRAMES}
+      fps={FPS}
+      width={1080}
+      height={1920}
+    />
+    <Composition
+      id="LegalVideo"
+      component={LegalVideo}
+      durationInFrames={360}
+      fps={30}
+      width={1080}
+      height={1920}
+      defaultProps={defaultLegalVideoProps}
+      calculateMetadata={calculateLegalVideoMetadata}
+    />
+  </>
 );
